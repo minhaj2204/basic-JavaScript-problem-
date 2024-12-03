c53e1abc9b273d21eba89f3c96a252199eef8966
minhaj2204 <minhajcartoons@gmail.com>
2024-12-03 18:13:37 +0600

    solving problem 3 ( with x = x * y )

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ let carName = "Valvo"
 
 
 let firstName = "John", lastName = "Doe", age = 35;
+
+
+// correct assignment operator and solving a funny math 
+
+
+let x = 10 ;
+
+let y = 5 ; 
+
+console.log( x = x * y)   // x value is 50 
