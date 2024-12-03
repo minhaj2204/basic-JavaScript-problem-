b2ed509c9d43bec8dd30bc7f8401269f04a1e0e6
minhaj2204 <minhajcartoons@gmail.com>
2024-12-03 21:15:32 +0600

    14 no question answering

---
+++ 14th.js
@@ -0,0 +1,12 @@
+// lowest number finding 
+
+
+function findSmallestNum ( arr ) {
+
+  return Math.min ( arr ) ;
+
+}
+
+console.log(findSmallestNum( [ 3, 5, 1, 9 ] ) ) ;  
+
+console.log(findSmallestNum( [ -1, -5, 0, 10 ]) ) ; 
