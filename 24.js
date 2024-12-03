749d0b1aee0fd44052390b5ff14598aabc5b6633
minhaj2204 <minhajcartoons@gmail.com>
2024-12-03 22:45:01 +0600

    getting the first letter of the string

---
+++ 24.js
@@ -0,0 +1,11 @@
+// getting the first letter of the string
+
+function getFirstCharacter (str) {
+
+  return str[0];
+
+}
+
+console.log( getFirstCharacter ( "hello" ) ) ;
+
+console.log( getFirstCharacter ( "world" ) ) ;
