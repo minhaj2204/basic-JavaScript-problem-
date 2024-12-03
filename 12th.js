e379df5c6f9045232c3606e23a4dd5499c692568
minhaj2204 <minhajcartoons@gmail.com>
2024-12-03 21:00:59 +0600

    Add files via upload

---
+++ 12th.js
@@ -0,0 +1,24 @@
+// temperature transformation
+
+
+function celsiusToFahrenheit(celsius) {
+  return (celsius * 9) / 5 + 32;
+}
+
+console.log(celsiusToFahrenheit(0));
+
+console.log(celsiusToFahrenheit(25));
+
+
+// even and odd cheacking
+
+
+function isEven ( num ) {
+
+  return num % 2 == 0 ;
+
+}
+
+console.log( isEven(4) )
+
+console.log( isEven(7) )
