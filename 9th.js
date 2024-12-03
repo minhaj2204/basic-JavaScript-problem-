b1da1bd934cc6e3986ae6939f9f75b19d5a9a6ef
minhaj2204 <minhajcartoons@gmail.com>
2024-12-03 20:10:40 +0600

    Add files via upload

---
+++ 9th.js
@@ -0,0 +1,20 @@
+// Random number
+
+
+let m = Math.random();
+
+console.log(m);
+
+
+// largest number in 10 to 20
+
+let n = Math.max(10, 20);
+
+console.log(n);
+
+
+// squre root
+
+let b = Math.sqrt(9);
+
+console.log(b);
