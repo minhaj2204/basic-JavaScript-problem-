671a36d4e77a3150aa62a8376bf8581d62eac449
minhaj2204 <minhajcartoons@gmail.com>
2024-12-03 18:23:45 +0600

    answer of the data type

--- script.js
+++ script.js
@@ -17,3 +17,18 @@ let x = 10 ;
 let y = 5 ; 
 
 console.log( x = x * y)   // x value is 50 
+
+
+// describeing data type with comments 
+
+
+let length = 16 ;             // length data type is number
+
+let lastName = "Johnson" ;                 // last name data type is string
+
+const x = {
+    
+    firstName : "John" ,
+    lastName : "Doe"
+
+} ;               // (const x)  data type is object
