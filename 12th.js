705aa949cf7d8db3b80eefa667af81f1a634fc40
minhaj2204 <minhajcartoons@gmail.com>
2024-12-03 21:02:14 +0600

    positive and negative check

--- 12th.js
+++ 12th.js
@@ -1,15 +1,3 @@
-// temperature transformation
-
-
-function celsiusToFahrenheit(celsius) {
-  return (celsius * 9) / 5 + 32;
-}
-
-console.log(celsiusToFahrenheit(0));
-
-console.log(celsiusToFahrenheit(25));
-
-
 // even and odd cheacking
 
 
