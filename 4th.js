0f7976e9023a909eef76ff4d904e2fdf1bfdeba2
minhaj2204 <minhajcartoons@gmail.com>
2024-12-03 19:16:46 +0600

    solving the 4th problem ( data type)

---
+++ 4th.js
@@ -0,0 +1,13 @@
+// describeing data type with comments 
+
+
+let length = 16 ;             // length data type is number
+
+let lastName = "Johnson" ;                 // last name data type is string
+
+const x = {
+
+    firstName : "John" ,
+    lastName : "Doe"
+
+} ;               // (const x)  data type is object
