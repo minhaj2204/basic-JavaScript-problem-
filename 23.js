fb7ca26d18801b431a574729b52b718895766716
minhaj2204 <minhajcartoons@gmail.com>
2024-12-03 22:39:27 +0600

    getting the last element of this arrays

---
+++ 23.js
@@ -0,0 +1,12 @@
+// getting the last element of this arrays
+
+
+function getLastElement (arr) {
+
+  return arr [arr.length - 1] ;
+
+}
+
+console.log( getLastElement ( [ 1, 2, 3 ] ) ) ; 
+
+console.log( getLastElement ( [ "a", "b", "c" ] ) ) ; 
