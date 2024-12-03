8f76c79c31344bef7a30979d8aa0019cf6a01d5b
minhaj2204 <minhajcartoons@gmail.com>
2024-12-03 22:22:47 +0600

    20 no question solution

---
+++ 20.js
@@ -0,0 +1,11 @@
+//  string to lowercase
+
+function toLowerCase (str) {
+
+  return str.toLowerCase();
+  
+}
+
+console.log( toLowerCase ("HELLO WORLD") ) ;
+
+console.log( toLowerCase ("JavaScript") ) ; 
