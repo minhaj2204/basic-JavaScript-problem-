0d967f6ecdb6a48f9ec65e91579883d2ec6df96e
minhaj2204 <minhajcartoons@gmail.com>
2024-12-03 22:19:21 +0600

    reversing word problem solve

---
+++ 19.js
@@ -0,0 +1,12 @@
+// reversing word
+
+
+function reverseString (str) { 
+
+  return [...str].reverse().join('') ;
+
+}
+
+console.log( reverseString ("hello") ) ; 
+
+console.log( reverseString ("world") ) ;
